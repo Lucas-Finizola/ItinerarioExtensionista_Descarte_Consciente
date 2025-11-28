726dd51c27ba4d4edc54b9907d23ce53d8e70a1c
Lucas-Finizola <lucasleitefcosta@gmail.com>
2025-11-27 23:36:49 -0300

    Configurando deploy e HashRouter

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/ItinerarioExtensionista_Descarte_Consciente/",
+
   server: {
     host: "::",
     port: 8080,
